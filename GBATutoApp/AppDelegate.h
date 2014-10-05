f6c78fd3814f6aa84ba90efc33b7d995295ab5e4
Grégory Bataille <gbataille.dev@gmail.com>
2014-10-05 15:26:25 +0200

    Initial commit

---
+++ GBATutoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GBATutoApp
+//
+//  Created by Gregory Bataille on 05/10/14.
+//  Copyright (c) 2014 Gregory Bataille. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
